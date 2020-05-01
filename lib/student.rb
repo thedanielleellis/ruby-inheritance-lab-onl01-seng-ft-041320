8f2860a6296a0c82ef01c22036918195dc8c20e9
Danielle Ellis <thedanielleellis@gmail.com>
2020-05-01 03:48:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,16 @@
-class Student < User 
+class Student < User
 
-attr_accessor :knowledge
+  attr_accessor :knowledge
 
-def initialize(knowledge)
-  knowledge = []
-end 
- 
- def learn(knowledge)
-   knowledge << knowledge
- end 
- 
- def knowledge
-   @knowledge 
- end 
- 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
